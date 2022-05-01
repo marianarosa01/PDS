1a8a8c191e99ea762f9c87a5b83475a9ac93952f
Mariana Rosa <72088400+marianarosa01@users.noreply.github.com>
2022-05-01 15:25:36 +0100

    cleaning

--- 98390_exameRecurso/Artigo.java
+++
@@ -1,7 +0,0 @@
-public interface Artigo {
-	public String name();
-	public String description();
-	public double price();
-	public void draw();
-      
-}
--- 98390_exameRecurso/StockManager.java
+++
@@ -1,7 +0,0 @@
-public interface StockManager extends Iterable<String> {
-
-	public boolean existeArtigo(String ref);
-	public boolean registaArtigo(String ref, Artigo p);
-	public Artigo removeArtigo(String ref);
-	
-}
